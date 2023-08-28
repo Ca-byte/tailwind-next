a700ff588ecc12081253cd962696693ca49d595a
Caroline Vieira <65242939+Ca-byte@users.noreply.github.com>
2023-08-28 22:36:29 +0200

    feat: animation portfolio - add filelist

--- src/app/components/Form/FileInput/ImagePreview.tsx
+++ src/app/components/Form/FileInput/ImagePreview.tsx
@@ -21,14 +21,14 @@ export function ImagePreview() {
         <User className="h-8 w-8 text-violet-500" />
       </div>
     )
+  } else {
+    return (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        src={previewURL}
+        alt=""
+        className="h-16 w-16 rounded-full object-cover"
+      />
+    )
   }
-
-  return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img
-      src={previewURL}
-      alt=""
-      className="h-16 w-16 rounded-full object-cover"
-    />
-  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -215,6 +215,7 @@ export default function Home() {
             </label>
             <FileInput.Root>
               <FileInput.Trigger />
+              <FileInput.FileList />
               <FileInput.Control multiple />
             </FileInput.Root>
           </div>
