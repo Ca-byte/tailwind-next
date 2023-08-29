f4ce11f3adcc04e863f3ffa57d2aa292203342a7
Caroline Vieira <65242939+Ca-byte@users.noreply.github.com>
2023-08-29 23:57:16 +0200

    feat: slots variants

--- src/app/components/Form/FileInput/FileList.tsx
+++ src/app/components/Form/FileInput/FileList.tsx
@@ -7,10 +7,18 @@ import { FileItem } from './FileItem'
 export function FileList() {
   const { files } = useFileInput()
   const [parent] = useAutoAnimate()
+
   return (
     <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => {
-        return <FileItem key={file.name} name={file.name} size={file.size} />
+        return (
+          <FileItem
+            key={file.name}
+            name={file.name}
+            size={file.size}
+            state="complete"
+          />
+        )
       })}
     </div>
   )
